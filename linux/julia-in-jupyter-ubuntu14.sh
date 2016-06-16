df6297003ff6bcf7dac2f535c2cc4a0a47904a07
Paul-St-Young <yyang173@illinois.edu>
2016-06-15 21:09:58 -0500

    tweaked julia install script

--- linux/julia-in-jupyter-ubuntu14.sh
+++ linux/julia-in-jupyter-ubuntu14.sh
@@ -6,20 +6,22 @@ sudo apt-get upgrade
 
 # scripting essentials
 # ------------------------------
-sudo apt-get install vim git
-sudo apt-get install python-dev
-#sudo apt-get install python-pip # this is lazy and bad but it works
-sudo pip install jupyter
+# dependencies
+#sudo apt-get install python-dev
+#sudo apt-get install python-pip
+#sudo pip -H install jupyter
 # InsecurePlatformWarning: either upgrade to python 2.7.9 or use pyOpenSSL
-sudo apt-get install gfortran
-sudo apt-get install liblapack-dev
-sudo pip install scipy
+#sudo apt-get install gfortran
+#sudo apt-get install liblapack-dev
+#sudo pip install scipy
 
+# install julia
 sudo add-apt-repository ppa:staticfloat/juliareleases
 sudo add-apt-repository ppa:staticfloat/julia-deps
 sudo apt-get update
 sudo apt-get install julia
 
+# setup in jupyter
 echo "Pkg.update()" | julia 
 echo "Pkg.add(\"IJulia\")" | julia # ? does not match cache
 echo "Pkg.build()" | julia
